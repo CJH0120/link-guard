f1d46e806a422f1c915d84fc46aa2d60fd7c693f
최종현 <rhrhdksk@gmail.com>
2024-08-05 17:26:47 +0900

    FEAT : 기능 추가

--- components/icons/ic-menu.tsx
+++ components/icons/ic-menu.tsx
@@ -1,18 +1,12 @@
-import * as React from "react"
-import { SVGProps, memo } from "react"
+import * as React from 'react';
+import { SVGProps, memo } from 'react';
 export const IconMenu = (props: SVGProps<SVGSVGElement>) => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="1em"
-		height="1em"
-		fill="none"
-		{...props}
-	>
-		<path
-			fill="currentColor"
-			d="M4 4h4v4H4V4ZM4 10h4v4H4v-4ZM8 16H4v4h4v-4ZM10 4h4v4h-4V4ZM14 10h-4v4h4v-4ZM10 16h4v4h-4v-4ZM20 4h-4v4h4V4ZM16 10h4v4h-4v-4ZM20 16h-4v4h4v-4Z"
-		/>
-	</svg>
-)
-const Memo = memo(IconMenu)
-export default Memo
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" {...props}>
+    <path
+      fill="currentColor"
+      d="M4 4h4v4H4V4ZM4 10h4v4H4v-4ZM8 16H4v4h4v-4ZM10 4h4v4h-4V4ZM14 10h-4v4h4v-4ZM10 16h4v4h-4v-4ZM20 4h-4v4h4V4ZM16 10h4v4h-4v-4ZM20 16h-4v4h4v-4Z"
+    />
+  </svg>
+);
+const Memo = memo(IconMenu);
+export default Memo;
--- components/icons/ic-setting.tsx
+++ components/icons/ic-setting.tsx
@@ -1,16 +1,9 @@
-import * as React from "react"
-import { SVGProps, memo } from "react"
+import * as React from 'react';
+import { SVGProps, memo } from 'react';
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		xmlSpace="preserve"
-		viewBox="0 0 32 32"
-		width="1em"
-		height="1em"
-		{...props}
-	>
-		<path d="M13 16c0 1.654 1.346 3 3 3s3-1.346 3-3-1.346-3-3-3-3 1.346-3 3zM13 26c0 1.654 1.346 3 3 3s3-1.346 3-3-1.346-3-3-3-3 1.346-3 3zM13 6c0 1.654 1.346 3 3 3s3-1.346 3-3-1.346-3-3-3-3 1.346-3 3z" />
-	</svg>
-)
-const IconSetting = memo(SvgComponent)
-export default IconSetting
+  <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 32 32" width="1em" height="1em" {...props}>
+    <path d="M13 16c0 1.654 1.346 3 3 3s3-1.346 3-3-1.346-3-3-3-3 1.346-3 3zM13 26c0 1.654 1.346 3 3 3s3-1.346 3-3-1.346-3-3-3-3 1.346-3 3zM13 6c0 1.654 1.346 3 3 3s3-1.346 3-3-1.346-3-3-3-3 1.346-3 3z" />
+  </svg>
+);
+const IconSetting = memo(SvgComponent);
+export default IconSetting;
