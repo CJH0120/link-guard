113938c9b81dbdb6ea2de892f027d68f32531e3b
최종현 <rhrhdksk@gmail.com>
2024-08-05 17:30:19 +0900

    FIX :타입 추가

--- app/api/auth/login/[...redirect]/route.ts
+++ app/api/auth/login/[...redirect]/route.ts
@@ -42,7 +42,7 @@ export async function GET(request: Request) {
         grant_type: 'authorization_code',
       }),
     });
-    const data: TokenResponse = await response.json();
+    const data: TokenResponse = (await response.json()) as TokenResponse;
 
     if (data.error) {
       console.error('Token Response Error:', data.error_description);
@@ -54,7 +54,11 @@ export async function GET(request: Request) {
         Authorization: `Bearer ${data.access_token}`,
       },
     });
-    const userInfo: { sub: string; name: string; email: string } = await userInfoResponse.json();
+    const userInfo: { sub: string; name: string; email: string } = (await userInfoResponse.json()) as {
+      sub: string;
+      name: string;
+      email: string;
+    };
 
     const member = await isMember({ google_id: userInfo.sub });
     if (!member) {
